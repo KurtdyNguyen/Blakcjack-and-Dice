499c19a5d29f21e157be37046cf49b9236ed00f8
KurtdyNguyen <leminhhlls2002@gmail.com>
2023-03-12 16:48:56 +0700

    exception handling

--- src/App/App.java
+++ src/App/App.java
@@ -1,11 +1,33 @@
 package App;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import objects.*;
 
 public class App {
+	public int nPlayer;
+	public gameround gameround;
+	
+	public App(){
+		System.out.println("\nEnter the number of players(from 1 to 4 players): ");
+		Scanner scan = new Scanner(System.in);
+		try {
+			int nPlayer = scan.nextInt();
+			while(nPlayer > 4 || nPlayer < 1) {
+				System.out.println("\nWrong input. Please enter the number of players again(1-4): ");
+				nPlayer = scan.nextInt();
+			}
+		} catch(InputMismatchException e){
+			System.out.println("\nInvalid input. Not an integer.");
+			e.printStackTrace();
+			scan.close();
+		}
+		scan.close();
+	}
 
 	public static void main(String[] args) {
-		
+		new App();
 	}
 
 }
